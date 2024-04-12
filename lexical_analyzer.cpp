74c446df72c6010db7412d6fd1a780be052fab96
Emad <emdhdri@gmail.com>
2024-04-13 01:10:08 +0330

    Update DFAs and complete Lexical Analyzer

--- lexical_analyzer.cpp
+++ lexical_analyzer.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
 using namespace std;
 
 bool is_letter(char c){
@@ -9,6 +9,10 @@ bool is_letter(char c){
 
 bool is_digit(char c){
     return c > 47 && c < 58;
+}
+
+bool is_white_space(char c){
+    return c == ' ' || c == '\n' || c == '\t';
 }
 
 int is_identifier(string code, int start){
@@ -27,7 +31,7 @@ int is_identifier(string code, int start){
                 else state = -1;
                 break;
             case 2:
-                return last_accept;
+                state = -1;
                 break;
             case 3:
                 if(is_letter(code[index])) state = 3;
@@ -39,21 +43,151 @@ int is_identifier(string code, int start){
                 else if(is_digit(code[index])) state = 4;
                 else state = -1;
                 break;
-            default:
+            case -1:
                 return last_accept;
+                break;
         }
         index++;
         if(state == 1 || state == 3 || state == 4){
-            last_accept = index;
+            last_accept = index - start;
         }
-        
     }
     return last_accept;
 }
+
+int is_number(string code, int start){
+    int index = start, state = 0;
+    int last_accept = 0;
+    while(index < code.length()){
+        switch(state){
+            case 0:
+                if(is_digit(code[index])) state = 1;
+                else state = -1;
+                break;
+            case 1:
+                if(is_digit(code[index])) state = 1;
+                else state = -1;
+                break;
+            case -1:
+                return last_accept;
+        }
+        index++;
+        if(state == 1){
+            last_accept = index - start;
+        }
+    }
+    return last_accept;
+}
+
+
+int is_reserved_word(string code, int start){
+    string reserved_words[14] = {"int", "float", "return", "if", "while", "cin", "cout", "continue", "break", "#include", "using", "namespace", "std", "main"};
+    int max_length = 0;
+    for(int i = 0; i < 14; i++){
+        string reserved_word = reserved_words[i];
+        int length = reserved_word.length();
+        if(code.substr(start, length) == reserved_word){
+            if(max_length < length){
+                max_length = length;
+            }
+        }
+    }
+    return max_length;
+}
+
+int is_symbol(string code, int start){
+    string symbols[23] = {"(", ")", "{", "}", "[", "]", ",", ";", "+", "-", "*", "/", "==", "!=", ">", ">=", "<", "<=", "=", "&&", "||", "<<", ">>"};
+    int max_length = 0;
+    for(int i = 0; i < 23; i++){
+        string symbol = symbols[i];
+        int length = symbol.length();
+        if(code.substr(start, length) == symbol){
+            if(max_length < length){
+                max_length = length;
+            }
+        }
+    }
+    return max_length;
+}
+
+int is_string(string code, int start){
+    int index = start, state = 0;
+    int last_accept = 0;
+    while(index < code.length()){
+        switch(state){
+            case 0:
+                if(code[index] == '"') state = 1;
+                else state = -1;
+                break;
+            case 1:
+                if(code[index] == '"') state = 2;
+                else state = 1;
+                break;
+            case 2:
+                if(code[index] == '"') state = 2;
+                else state = 1;
+                break;
+            case -1:
+                return last_accept;
+        }
+        index++;
+        if(state == 2){
+            last_accept = index - start;
+        }
+    }
+    return last_accept;
+}
+
+vector<pair<string, string>> lexical_analyze(string code){
+    cout << code << endl;
+    int start = 0;
+    vector<pair<string, string>> tokens;
+    while(start < code.length()){
+        int max_match = 0;
+        string token = "";
+        int rw_match = is_reserved_word(code, start);
+        if(rw_match > max_match){
+            max_match = rw_match;
+            token = "reservedword";
+        }
+        int id_match = is_identifier(code, start);
+        if(id_match > max_match){
+            max_match = id_match;
+            token = "identifier";
+        }
+        int number_match = is_number(code, start);
+        if(number_match > max_match){
+            max_match = number_match;
+            token = "number";
+        }
+        int symbol_match = is_symbol(code, start);
+        if(symbol_match > max_match){
+            max_match = symbol_match;
+            token = "symbol";
+        }
+        int string_match = is_string(code, start);
+        if(string_match > max_match){
+            max_match = string_match;
+            token = "string";
+        }
+        if(!max_match){
+            start++;
+        }
+        else{
+            string token_value = code.substr(start, max_match);
+            tokens.push_back(make_pair(token, token_value));
+            
+            start += max_match;
+        }
+    }
+    return tokens;
+}
+
 int main(){
     string code;
-    cin >> code;
-    int length = is_identifier(code, 0);
-    cout << code.substr(0, length);
+    code = "#include<iostream>\nusing namespace std;\nint main () {\n    int x;\n    int s=0, t=10;\n    while(t>=0){\n        cin>>x;\n    t = t-113134440\n    s = s + x\n    }\n    cout<<\"sum=\"<<s;\n    return 0;\n}";
+    for(auto x: lexical_analyze(code)){
+        cout << "<" << x.first << ", " << x.second << ">" << endl;
+    }
     return 0;
 }
